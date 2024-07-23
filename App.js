10a0fad1c0c78228e231477a103c6f78fd18bbff
mahajanruhi <89562068+mahajanruhi@users.noreply.github.com>
2024-07-23 13:31:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+ import Deed from './Deed.jsx';
+//* import Verfication from './Verfication.jsx';
+ //*  import Doc from './Doc.jsx';
+ //*   import Power from './Power';
+function App() {
+  return (
+    <> 
+    <div className="flex">
+    <Deed/>
+   {/*  <Verfication/> */ }
+     {/* <Doc/>  */ }
+   {/*  <Power/>  */ }
+
+    </div>
+    </>
+  );
+}
+
+ export default App;
